041527816e971f30a5177a92e7b6cb172e7812b5
Patrese <patrese.ebs@gmail.com>
2026-04-17 15:56:00 -0400

    fix: usar CYPRESS_BASE_URL para sobrescrever baseUrl no CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env['CYPRESS_baseUrl'] ?? 'https://manuos.izitecnologia.com.br',
+    baseUrl: 'https://manuos.izitecnologia.com.br',
     viewportWidth: 1280,
     viewportHeight: 720,
     defaultCommandTimeout: 10000,
